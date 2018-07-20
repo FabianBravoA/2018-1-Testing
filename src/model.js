90cf0168f716255d6cb0046229a18a01ccdeaf0e
Fabian Alexis Bravo Abarca <fabian.bravo.abarca@gmail.com>
2018-07-20 12:20:13 -0400

    Nuevos tests

--- src/model.js
+++ src/model.js
@@ -1,10 +1,32 @@
-window.addTask = (title) =>{
+window.addTask = (title) => {
     const newTaskKey = firebase.database().ref().child('todo').push().key;
-    return firebase.database().ref('todo/'+newTaskKey).set({
-        title : title
+    return firebase.database().ref('todo/' + newTaskKey).set({
+        title: title
     });
 };
 
-window.getTaskList = () =>{
+window.getTaskList = () => {
     return firebase.database().ref('todo').once('value');
+}
+
+window.taskProgress = (title, state) => {
+    return firebase.database().ref('todo').once("value").then(
+        (results) => {
+            const taskKey = Object.entries(results.val()).find(
+                task => task[1].title == title
+            )[0];
+            return firebase.database().ref('todo/' + taskKey).update({
+                state: state
+            });
+        }
+    ).then(() => {
+        return firebase.database().ref('todo').once("value").then(
+            (results) => {
+                let taskToReturn = Object.entries(results.val()).find(
+                    task => task[1].title == title
+                )[1];
+                return taskToReturn;
+            }
+        )
+    });
 }
--- test/model.js
+++ test/model.js
@@ -11,37 +11,38 @@ var config = {
     projectId: "test-todolist-10d7b",
     storageBucket: "test-todolist-10d7b.appspot.com",
     messagingSenderId: "552577706745"
-  };
+};
 firebase.initializeApp(config);
 
 describe(
-    "Modelo de la lista de tareas",
-    ()=>{
+    "Modelo de la lista de tareas",// se empieza por lo general
+    () => {
         describe(
             "La lista, me debería permitir agregar tareas",
-            ()=>{
+            () => {
                 it("Debería agregar una tarea",
-                    (done)=>{
+                    (done) => {
                         addTask("Comprar pan").then(
-                            (task)=>{
-                                return getTaskList(); 
+                            (task) => {
+                                return getTaskList();
                             }
                         ).then(
-                            (taskList)=>{
+                            (taskList) => {
                                 const comprarPan = Object.entries(taskList.val())
                                     .find(
                                         task => {
                                             return task[1].title == "Comprar pan";
                                         }
                                     );
-                                assert.exists(comprarPan[1]);
+                                assert.exists(comprarPan[1]);//verifica que exista algo en particular en el código una función
                                 assert.equal(comprarPan[1].title, "Comprar pan");
+                                done();
                             }
                         ).catch(
-                            (error)=>{
-                                assert.error();
+                            (error) => {
+                                done(error);
                             }
-                        ).finally(done);
+                        )
                     }
                 );
             }
@@ -49,21 +50,34 @@ describe(
 
         describe(
             "La lista, me debería permitir colocarle un progreso a una tarea",
-            ()=>{
-
+            () => {
+                it("Debería permitirle colocarle progreso a una tarea",
+                    (done) => { //parametros de la función
+                        taskProgress("Comprar pan", "se ha comprado").then(
+                            (task) => {
+                                assert.exists(task);
+                                assert.equal(task.title, "Comprar pan");
+                                assert.equal(task.state, "se ha comprado");
+                                done();
+                            }
+                        ).catch(
+                            (error) => {
+                                done(error);
+                            }
+                        );
+                    })
             }
         );
 
         describe(
             "La lista, me debería permitir editar una tarea",
-            ()=>{
-
+            () => {
             }
         );
 
         describe(
             "La lista, me debería permitir borrar una tarea",
-            ()=>{
+            () => {
 
             }
         );
